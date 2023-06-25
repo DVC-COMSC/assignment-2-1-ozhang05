955234ebc7515abad621d114ec5105fc56d35c18
Oliver <ozhang2305@gmail.com>
2023-06-24 23:46:32 -0700

    Finished

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ def main():
     ##################################################
     """
 
+    males = input("Input the number of males: ")
+    females = input("Input the number of females: ")
+    total = int(males) + int(females)
+    m_perc = int(males)/total
+    f_perc = int(females)/total
+
+    print ("Total number of students: " + str(total))
+    print ("The number of males and females: " + str(males) + ", " + str(females))
+    print ("The percentages of males and females: " + str(format(m_perc, '.2%')) + ", " + str(format(f_perc, '.2%')))
+
     """
     ########################################
     # Do not delete the return statement
